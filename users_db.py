4c03a50dc67c91d110a74083896984feb488e9df
Костя Кондрашов <desanto@inbox.ru>
2021-02-06 16:59:03 +0200

    добавил методы и обновил старые 2

--- users_db.py
+++ users_db.py
@@ -20,17 +20,19 @@ class UsersDB(DB):
         if not self.is_user_in_db(sn_type=sn_type, u_id=u_id):
             self.cur.execute("INSERT INTO users({}) VALUES ({})".format(sn_type, u_id))
             self.db.commit()
+        self.print_table("users")
 
     @correct_sn
     def add_tiktok_to_user(self, sn_type=None, u_id=None, tt_nm=None):
-        self.cur.execute("""UPDATE users SET tt_nm = {} 
-                            WHERE ? = ?""".format(tt_nm, sn_type, u_id))
+        self.cur.execute(
+            """UPDATE users SET tt_nm = \"{}\" WHERE {} = {}""".format(tt_nm, sn_type, u_id))
         self.db.commit()
+        self.print_table("users")
 
     @correct_sn
     def is_tiktok(self, sn_type=None, u_id=None):
-        return bool(self.cur.execute("""SELECT * FROM users 
-                                        WHERE {} = {} and tt_nm != NULL""".format(sn_type, u_id)))
+        return bool(self.cur.execute("""SELECT * FROM users WHERE {} = {} AND tt_nm != NULL""".
+                                     format(sn_type, u_id)).fetchall())
 
 
 if __name__ == "__main__":
