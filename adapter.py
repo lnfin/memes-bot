7ede303d0519ea10cdac6aa7985d6df07a38741e
Bermy <63257415+Bermy@users.noreply.github.com>
2021-02-06 23:51:42 +0300

    Выкинул Мусор, добавил комментарии

--- adapter.py
+++ adapter.py
@@ -1,34 +1,16 @@
 from filename import database as Db
 
-TT_ID = ''
-TG_ID = ''
-VK_ID = ''
-
 
 class Adapter:
     def __init__(self):
         self.db = Db()
 
-    def add_user(self, vk_id='', tg_id='', tt_id=''):  # TODO Вероятно, это тоже придётся немного изменить
-        if tg_id:
-            self.db.add_user(sn_type=TG_ID, u_id=tg_id)
-            if tt_id:
-                self.db.add_tiktok_to_user(sn_type=TG_ID, u_id=tg_id)
-        elif vk_id:
-            self.db.add_user(sn_type=VK_ID, u_id=vk_id)
-            if tt_id:
-                self.db.add_tiktok_to_user(sn_type=VK_ID, u_id=vk_id)
-
-    def add_tiktok_to_user(self, sn_type=None, u_id=None):
-        self.db.add_tiktok_to_user(sn_type=sn_type, u_id=u_id)
-
-    def found_vk_friends(self, user_id, table=VK_ID):
+    def get_vk_friends(self, user_id):
+        """Возвращает 5 человек с общими интересами"""
         unions = []
         user_vk_id =
         user_tg_id =        # TODO Получение этих данных юзера
-        user_vk_data =
-        user_vk_data = set(user_vk_data.split(' '))
-
+        user_vk_data =      # TODO Получение интересов пользователя
         users = # TODO Метод получения всех юзеров
         for user in users:
             other_id = user[0]
@@ -39,25 +21,7 @@ class Adapter:
 
             union = other_vk_data & user_vk_data
             if union > 0:
-                unions.append(((other_tg_id, other_vk_id), union))
+                unions.append(other_tg_id, union)
 
         unions.sort(key= lambda x: x[1], reverse=True)
         return unions[:5]
-
-    def get_found_vk_friends(self, user_id, table=VK_ID):
-        ans = []
-        unions = self.found_vk_friends(user_id, table)
-        for other_ids, match in unions:
-            other_vk = other_ids[1]
-            other_tg = other_ids[0]
-            if table == VK_ID:
-                if other_vk:
-                    ans.append((f'vk.com{other_vk}', match))
-                else:
-                    pass # TODO перевод id тг в сслыку
-
-            elif table == TG_ID:
-                if other_tg:
-                    pass # TODO тут тоже
-                else:
-                    ans.append((f'vk.com{other_vk}', match))
