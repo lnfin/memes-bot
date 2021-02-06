d83e543773efdcd0823a6c8460528c5eb882c825
Bermy <63257415+Bermy@users.noreply.github.com>
2021-02-06 22:05:27 +0300

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -34,7 +34,22 @@ class UsersDB(DB):
         return bool(self.cur.execute("""SELECT * FROM users WHERE {} = {} AND tt_nm != NULL""".
                                      format(sn_type, u_id)).fetchall())
 
+    def check_test_matches(self, test):
+        result = self.cur.execute(f"""SELECT vk_id, tg_id, test_result FROM users
+                                    WHERE test result IS NOT NULL""").fetchall()
+        ans = []
+        for user in result:
+            vk_id = user[0]
+            tg_id = user[1]
+            other_test = user[2]
+            match = list(map(lambda x: 1 if x else 0, (other_test[i] == test[i] for i in range(10))))
+            cost = sum(match)
+            if cost > 0:
+                ans.append(((vk_id, tg_id), cost))
+        ans.sort(key=lambda x: x[1], reverse=True)
+        return ans[:10]
 
+        
 if __name__ == "__main__":
     db = UsersDB()
     db.print_table("users")
