af2c789cfcd05b56ebb35011608e695757b5ced9
Bermy <63257415+Bermy@users.noreply.github.com>
2021-02-07 00:26:38 +0300

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -1,31 +1,7 @@
 import sqlite3
 import os
 
-
-def correct_sn(method):
-    sns = ("vk", "tg")
-
-    def check_condition(self, sn_type=None, u_id=None, **kwargs):
-        if sn_type in sns: sn_type += "_id"
-        if sn_type in [sn + "_id" for sn in sns]:
-            return method(self, sn_type=sn_type, u_id=u_id, **kwargs)
-        else:
-            raise Exception
-
-    return check_condition
-
-
-class DB:
-    def __init__(self):
-        self.db = None
-        self.cur = None
-
-    def get_table(self, table):
-        return self.cur.execute("SELECT * FROM {}".format(table)).fetchall()
-
-    def print_table(self, table):
-        print(*self.get_table(table), sep="\n")
-
+from db import DB, correct_sn
 
 
 class UsersDB(DB):
