3e5b60eb342e5a5538cbac1d1c431f8b5d652d33
Bermy <63257415+Bermy@users.noreply.github.com>
2021-02-07 10:06:42 +0300

    Update adapter.py

--- adapter.py
+++ adapter.py
@@ -5,23 +5,13 @@ class Adapter:
     def __init__(self):
         self.db = Db()
 
-    def get_vk_friends(self, user_id):
-        """Возвращает 5 человек с общими интересами"""
-        unions = []
-        user_vk_id =
-        user_tg_id =        # TODO Получение этих данных юзера
-        user_vk_data =      # TODO Получение интересов пользователя
-        users = # TODO Метод получения всех юзеров
-        for user in users:
-            other_id = user[0]
-            other_vk_id = user[1]
-            other_tg_id = user[2]
-            other_vk_data = user[3]
-            other_vk_data = set(other_vk_data.split(' '))
-
-            union = other_vk_data & user_vk_data
-            if union > 0:
-                unions.append(other_tg_id, union)
-
-        unions.sort(key= lambda x: x[1], reverse=True)
-        return unions[:5]
+    def get_vk_friends(self, user_id, subscribes):
+        matches = self.db.get_get_subscribe_match(subscribes)
+        ans = []
+        while True:
+            user = matches.pop[0]
+            if user[0] != user_id:
+                ans.append(user)
+            if len(ans) == 5:
+                break
+        return ans
